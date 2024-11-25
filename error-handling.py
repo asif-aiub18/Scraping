219c2ef046e19901d4e004487233fd08106b65a6
asif-aiub18 <asifanower24@gmail.com>
2024-11-25 21:23:46 +0600

    update

--- error-handling.py
+++ error-handling.py
@@ -8,13 +8,21 @@
 #         print("The element is not a number!")
 
 #While-Break
-n=4
-while n > 0:
-    print(n)
-    n = n-1
-    if n==2:
-        break
+# n=4
+# while n > 0:
+#     print(n)
+#     n = n-1
+#     if n==2:
+#         break
 # while <expr>:
 #     <statement>
+
+
+n1=5
+while n1 > 5:
+     print(n1)
+     n1 = n1-1
+     if n1==3:
+         break
 
 print('Loop ended')
