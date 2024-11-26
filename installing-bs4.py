e82fd36dc2f7925cc304c20e7f790cd38d995c59
asif-aiub18 <asifanower24@gmail.com>
2024-11-26 16:48:13 +0600

    How to get the html from a website

--- installing-bs4.py
+++ installing-bs4.py
@@ -1,3 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
+website = 'https://subslikescript.com/movie/Titanic-120338'
+response = requests.get(website)
+content= response.text
+
+soup = BeautifulSoup(content, 'lxml')
+print(soup.prettify())
